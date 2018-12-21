3694d190424069c7c8c3d73dca157a58dd849934
Arnas Kromelis <arnas.kromelis@gmail.com>
2018-12-21 14:15:43 +0400

    testing build scenarios
    test builds

--- projects/projectB/__tests__/index-test.js
+++ projects/projectB/__tests__/index-test.js
@@ -3,3 +3,7 @@ const sum = require('../index.js');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 30 + -120 to equal -90', () => {
+  expect(sum(30, -120)).toBe(-90);
+});
